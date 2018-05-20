026bfc00db6c4c0a84488865e4ef30b43ae5609c
aleph1507 <xrristo@gmail.com>
2018-05-20 19:16:14 +0200

    res_ipn0001

--- app/Http/Controllers/SellsController.php
+++ app/Http/Controllers/SellsController.php
@@ -101,6 +101,22 @@ class SellsController extends Controller
 
       if ($response == 'VERIFIED') {
           // Your code goes here ...
+          $ch = curl_init('https://ipnpb.paypal.com/cgi-bin/webscr');
+          curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+          curl_setopt($ch, CURLOPT_POST, 1);
+          curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+          curl_setopt($ch, CURLOPT_POSTFIELDS, $req);
+          curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
+          curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+          curl_setopt($ch, CURLOPT_FORBID_REUSE, 1);
+          curl_setopt($ch, CURLOPT_HTTPHEADER, array('Connection: Close'));
+
+          if ( !($res = curl_exec($ch)) ) {
+            // error_log("Got " . curl_error($ch) . " when processing IPN data");
+            curl_close($ch);
+            exit;
+          }
+
           $s = new Sell();
           $s->textReport = "vo VERIFIED";
           $s->save();
