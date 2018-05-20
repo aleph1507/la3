aa5c14ab6f35dc6d5d86ae392c5644e67f210577
aleph1507 <xrristo@gmail.com>
2018-05-20 20:40:02 +0200

    res_ipn0001

--- public/js/checkcoupon.js
+++ public/js/checkcoupon.js
@@ -100,14 +100,23 @@ function checkCoupons(e) {
       // console.log("vo proverka e.target.value : " + e.target.value);
       if(e.target.value == this.coupons[i]){
         console.log("COUPON : " + this.coupons[i]);
-        couponpaypal = "<form action=\"https://www.paypal.com/cgi-bin/webscr\" method=\"post\" target=\"_top\"" + ">" +
+        // couponpaypal = "<form action=\"https://www.paypal.com/cgi-bin/webscr\" method=\"post\" target=\"_top\"" + ">" +
+        //                   "<input type=\"hidden\" name=\"custom\" value=\"" + this.coupons[i] + "\">" +
+        //                   "<input type=\"hidden\" name=\"cmd\" value=\"_s-xclick\"" + ">" +
+        //                   "<input type=\"hidden\" name=\"hosted_button_id\" value=\"H9MCY5DU4RNSA\"" + ">" +
+        //                   "<input type=\"image\" src=\"https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif\"" +
+        //                     "width=\"500px\" border=\"0\" name=\"submit\" alt=\"PayPal - The safer, easier way to pay online!\"" + ">" +
+        //                   "<img alt=\"\" border=\"0\" src=\"https://www.paypalobjects.com/en_US/i/scr/pixel.gif\" width=\"1\" height=\"1\"" + ">" +
+        //                 "</form>";
+
+        couponpaypal = "<form action=\"https://www.sandbox.paypal.com/cgi-bin/webscr\" method=\"post\" target=\"_top\">" +
                           "<input type=\"hidden\" name=\"custom\" value=\"" + this.coupons[i] + "\">" +
-                          "<input type=\"hidden\" name=\"cmd\" value=\"_s-xclick\"" + ">" +
-                          "<input type=\"hidden\" name=\"hosted_button_id\" value=\"H9MCY5DU4RNSA\"" + ">" +
-                          "<input type=\"image\" src=\"https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif\"" +
-                            "width=\"500px\" border=\"0\" name=\"submit\" alt=\"PayPal - The safer, easier way to pay online!\"" + ">" +
-                          "<img alt=\"\" border=\"0\" src=\"https://www.paypalobjects.com/en_US/i/scr/pixel.gif\" width=\"1\" height=\"1\"" + ">" +
+                          "<input type=\"hidden\" name=\"cmd\" value=\"_s-xclick\">" +
+                          "<input type=\"hidden\" name=\"hosted_button_id\" value=\"8K4MCRQH42JHA\">" +
+                          "<input type=\"image\" src=\"https://www.sandbox.paypal.com/en_US/i/btn/btn_buynowCC_LG.gif\" border=\"0\" name=\"submit\" alt=\"Love Abloom with a coupon!\">" +
+                          "<img alt=\"\" border=\"0\" src=\"https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif\" width=\"1\" height=\"1\">" +
                         "</form>";
+
         document.getElementById('paypal').innerHTML = couponpaypal;
         cpaypal = true;
         console.log("COUPON");
